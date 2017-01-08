34a0dd1ff5d503fec5f163534868189def06f351
Kostub Deshmukh <kostub@gmail.com>
2017-01-08 13:38:38 -0800

    Support various font styles for MTMathAtom
    This includes rendering support for the following styles:
    Roman, Italic, Bold, BoldItalic, Caligraphic, Typewriter, SansSerif,
    Blackboard, Fraktur.
    
    This in preparation for supporting the following latex commands:
    \mathrm, \mathtt, \mathbf, \mathit, \mathcal, \mathsf, \mathbb,
    \mathfrak and \text.
    
    This is the first step for #12.

--- iosMath/lib/MTUnicode.m
+++ iosMath/lib/MTUnicode.m
@@ -11,16 +11,6 @@
 
 #import "MTUnicode.h"
 
-const unichar kMTUnicodeCapitalGreekStart = 0x0391;
-const unichar kMTUnicodeCapitalGreekEnd = 0x03A9;
-const unichar kMTUnicodeGreekStart = 0x03B1;
-const unichar kMTUnicodeGreekEnd = 0x03C9;
-const unichar kMTUnicodePlanksConstant = 0x210e;
-const UTF32Char kMTUnicodeMathItalicStart = 0x1D44E;
-const UTF32Char kMTUnicodeMathCapitalItalicStart = 0x1D434;
-const UTF32Char kMTUnicodeGreekMathItalicStart = 0x1D6FC;
-const UTF32Char kMTUnicodeGreekMathCapitalItalicStart = 0x1D6E2;
-
 @implementation NSString (Unicode)
 
 
