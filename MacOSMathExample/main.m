3034dc5fb335402d9c7f51ceeac9cccb716ac641
安志钢 <AnZhg@MacBook-Pro.local>
2017-01-09 17:13:52 -0500

    Add missing license. Add Mac example to workspace.

--- MacOSMathExample/main.m
+++ MacOSMathExample/main.m
@@ -5,6 +5,9 @@
 //  Created by 安志钢 on 17-01-08.
 //  Copyright © 2017年 安志钢. All rights reserved.
 //
+//  This software may be modified and distributed under the terms of the
+//  MIT license. See the LICENSE file for details.
+//
 
 #import <Cocoa/Cocoa.h>
 
