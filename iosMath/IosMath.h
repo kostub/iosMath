179227efff91971d87faa4adba26df5c175b62ae
Ahmed Elkady <ahmed.elkady@tophatmonocle.com>
2017-09-20 16:17:17 -0400

    Update CocoaPods + header file to get project to build

--- iosMath/IosMath.h
+++ iosMath/IosMath.h
@@ -8,7 +8,7 @@
 //  This software may be modified and distributed under the terms of the
 //  MIT license. See the LICENSE file for details.
 
-#import <IosMath/MTMathUILabel.h>
-#import <IosMath/MTMathListDisplay.h>
-#import <IosMath/MTMathList.h>
-#import <IosMath/MTMathListBuilder.h>
+#import <iosMath/render/MTMathUILabel.h>
+#import <iosMath/render/MTMathListDisplay.h>
+#import <iosMath/lib/MTMathList.h>
+#import <iosMath/lib/MTMathListBuilder.h>
