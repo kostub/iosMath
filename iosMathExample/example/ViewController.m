ab018eb1239ccda2cd0fe25bbbb0f3a7affaf229
User <User@vms-4.hq.telesens.lan>
2018-10-01 18:40:57 +0300

    edited text

--- iosMathExample/example/ViewController.m
+++ iosMathExample/example/ViewController.m
@@ -87,7 +87,7 @@
 
     // Demo formulae
     // Quadratic formula
-    self.demoLabels[0] = [self createMathLabel:@"ваш вопрос your question: x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}" withHeight:60];
+    self.demoLabels[0] = [self createMathLabel:@"\\text{ваш вопрос your question: }x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}" withHeight:60];
     [self addLabelAsSubview:self.demoLabels[0] to:contentView];
     self.demoLabels[0].fontSize = 15;
     // This is first label so set the height from the top
