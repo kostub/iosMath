516e164422249c24ee997ca06765216f1e2c990e
Nawras GEORGI <admin@nawrasg.fr>
2020-07-29 21:21:01 +0200

    Add path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "iosMath",
-            dependencies: []),
+            dependencies: [],
+            path: "./"),
         .testTarget(
             name: "iosMathTests",
             dependencies: ["iosMath"]),
